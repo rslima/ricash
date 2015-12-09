8a665692ecc60e9029815dbe418afd43adec271f
Ricardo da Silva Lima <rslima@senado.gov.br>
2015-12-09 09:28:03 -0200

    Initial version

---
+++ src/test/java/com/rslima/RicashApplicationTests.java
@@ -0,0 +1,18 @@
+package com.rslima;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = RicashApplication.class)
+@WebAppConfiguration
+public class RicashApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
