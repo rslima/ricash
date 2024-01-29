82ffa7f02266846d5a4223849c0920b9f6e7fb12
Ricardo da Silva Lima <rslima@gmail.com>
2024-01-29 06:15:00 -0300

    chore: update project

--- src/main/java/com/rslima/ricash/RicashApplication.java
+++ src/main/java/com/rslima/ricash/RicashApplication.java
@@ -2,13 +2,12 @@ package com.rslima.ricash;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class RicashApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(RicashApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(RicashApplication.class, args);
+	}
+
 }
