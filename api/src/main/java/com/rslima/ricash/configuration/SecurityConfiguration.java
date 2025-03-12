847c9dbe1bf61f917b170911d97e9e327b38d009
Ricardo da Silva Lima <rslima@senado.leg.br>
2025-03-12 11:42:53 -0300

    Configure app as resource server

--- api/src/main/java/com/rslima/ricash/configuration/SecurityConfiguration.java
+++ api/src/main/java/com/rslima/ricash/configuration/SecurityConfiguration.java
@@ -2,11 +2,17 @@ package com.rslima.ricash.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @EnableWebSecurity
 @Configuration
@@ -18,10 +24,26 @@ public class SecurityConfiguration {
                         authorizeRequests
                                 .requestMatchers(GET,"/index.html").permitAll()
                                 .anyRequest().authenticated())
-                .oauth2Login(login ->
-                        login.loginPage("/oauth2/authorization/keycloak")
-                                .defaultSuccessUrl("/"))
+                .oauth2ResourceServer(rs -> rs.jwt(
+                        jwt -> jwt.jwtAuthenticationConverter(jwtConverter())
+                ))
+                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
 
                 .build();
     }
+
+    @Bean
+    public Converter<Jwt, AbstractAuthenticationToken> jwtConverter() {
+
+        final var grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("realm_access/roles");
+        grantedAuthoritiesConverter.setAuthorityPrefix("");
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setPrincipalClaimName("preferred_username");
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+
+        return jwtAuthenticationConverter;
+    }
+
 }
