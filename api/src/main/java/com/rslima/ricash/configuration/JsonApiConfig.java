45040de9e0f9679cd0f989d1d055e8075fb99143
Ricardo da Silva Lima <rslima@senado.leg.br>
2025-03-28 09:16:17 -0300

    Update Spring Boot version

--- api/src/main/java/com/rslima/ricash/configuration/JsonApiConfig.java
+++ api/src/main/java/com/rslima/ricash/configuration/JsonApiConfig.java
@@ -1,6 +1,5 @@
 package com.rslima.ricash.configuration;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.toedter.spring.hateoas.jsonapi.JsonApiConfiguration;
 import org.springframework.context.annotation.Bean;
@@ -9,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class JsonApiConfig {
     @Bean
-    public JsonApiConfiguration jsonApiConfiguration(ObjectMapper objectMapper) {
+    public JsonApiConfiguration jsonApiConfiguration() {
         return new JsonApiConfiguration()
                 .withObjectMapperCustomizer(om -> om.registerModule(new JavaTimeModule()));
     }
