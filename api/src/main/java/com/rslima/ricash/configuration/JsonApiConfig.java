71211f7e028a8b339f223733fe022194857b657e
Ricardo da Silva Lima <rslima@gmail.com>
2026-01-20 09:03:07 -0300

    remove unused JavaTimeModule registration in JsonApiConfig

--- api/src/main/java/com/rslima/ricash/configuration/JsonApiConfig.java
+++ api/src/main/java/com/rslima/ricash/configuration/JsonApiConfig.java
@@ -1,6 +1,5 @@
 package com.rslima.ricash.configuration;
 
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.toedter.spring.hateoas.jsonapi.JsonApiConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,7 +8,6 @@ import org.springframework.context.annotation.Configuration;
 public class JsonApiConfig {
     @Bean
     public JsonApiConfiguration jsonApiConfiguration() {
-        return new JsonApiConfiguration()
-                .withObjectMapperCustomizer(om -> om.registerModule(new JavaTimeModule()));
+        return new JsonApiConfiguration();
     }
 }
