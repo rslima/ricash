8e48daa70067af2732477a76ef2edd0c9c1df7a9
Ricardo da Silva Lima <rslima@gmail.com>
2026-01-20 10:15:42 -0300

    add CRUD operations to UserController with JWT security integration

--- api/src/main/java/com/rslima/ricash/users/UserController.java
+++ api/src/main/java/com/rslima/ricash/users/UserController.java
@@ -9,10 +9,13 @@ import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.PagedModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -30,58 +33,100 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
         this.userService = userService;
     }
 
-    @GetMapping PagedModel<EntityModel<UserResource>> listUsers(
+    @GetMapping
+    public PagedModel<EntityModel<UserResource>> listUsers(
+            JwtAuthenticationToken principal,
             @RequestParam(name = "page[number]", required = false, defaultValue = "0") int page,
             @RequestParam(name = "page[size]", required = false, defaultValue = "20") int size) {
 
         final var pageable = PageRequest.of(page, size);
-
-        var userResources = userService.listUsers(pageable).map(this::toUserResource);
-
+        var userResources = userService.listUsers(pageable).map(this::toEntityModel);
 
         return buildPagedUserResponse(page, size, userResources);
     }
 
-    private @NotNull PagedModel<EntityModel<UserResource>> buildPagedUserResponse(
+    @GetMapping("/{id}")
+    public EntityModel<UserResource> getUser(
+            JwtAuthenticationToken principal,
+            @PathVariable String id) {
+
+        log.debug("User {} fetching user {}", principal.getName(), id);
+        return userService.findUser(id)
+                .map(this::toEntityModel)
+                .orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<EntityModel<UserResource>> createUser(
+            JwtAuthenticationToken principal,
+            @Valid @RequestBody CreateUserRequest request) {
+
+        log.debug("User {} creating new user with email {}", principal.getName(), request.email());
+        User createdUser = userService.createUser(request);
+        EntityModel<UserResource> createdUserEntity = toEntityModel(createdUser);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUserEntity);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EntityModel<UserResource>> updateUser(
+            JwtAuthenticationToken principal,
+            @PathVariable String id,
+            @Valid @RequestBody UpdateUserRequest request) {
+
+        log.debug("User {} updating user {}", principal.getName(), id);
+        User updatedUser = userService.updateUser(id, request);
+        EntityModel<UserResource> updatedUserEntity = toEntityModel(updatedUser);
+
+        return ResponseEntity.ok(updatedUserEntity);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteUser(
+            JwtAuthenticationToken principal,
+            @PathVariable String id) {
+
+        log.debug("User {} deleting user {}", principal.getName(), id);
+        userService.deleteUser(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
             int page,
             int size,
             Page<EntityModel<UserResource>> userResources) {
-        var entityModels = PagedModel.of(
-                userResources.getContent(), new PagedModel.PageMetadata(
+
+        var pagedModel = PagedModel.of(
+                userResources.getContent(),
+                new PagedModel.PageMetadata(
                         userResources.getSize(),
                         userResources.getNumber(),
                         userResources.getTotalElements(),
                         userResources.getTotalPages()));
 
-        entityModels.add(linkTo(methodOn(UserController.class).listUsers(page, size)).withSelfRel());
-        entityModels.add(linkTo(methodOn(UserController.class).listUsers(0, size)).withRel("first"));
-        entityModels.add(linkTo(methodOn(UserController.class).listUsers(
-                userResources.getTotalPages() - 1,
-                size)).withRel("last"));
+        pagedModel.add(linkTo(methodOn(UserController.class).listUsers(null, page, size)).withSelfRel());
+        pagedModel.add(linkTo(methodOn(UserController.class).listUsers(null, 0, size)).withRel("first"));
+
+        if (userResources.getTotalPages() > 0) {
+            pagedModel.add(linkTo(methodOn(UserController.class).listUsers(
+                    null,
+                    userResources.getTotalPages() - 1,
+                    size)).withRel("last"));
+        }
         if (userResources.hasNext()) {
-            entityModels.add(linkTo(methodOn(UserController.class).listUsers(
+            pagedModel.add(linkTo(methodOn(UserController.class).listUsers(
+                    null,
                     userResources.getNumber() + 1,
                     size)).withRel("next"));
         }
         if (userResources.hasPrevious()) {
-            entityModels.add(linkTo(methodOn(UserController.class).listUsers(
+            pagedModel.add(linkTo(methodOn(UserController.class).listUsers(
+                    null,
                     userResources.getNumber() - 1,
                     size)).withRel("prev"));
         }
-        return entityModels;
-    }
 
-    @GetMapping("/{id}") EntityModel<UserResource> getUser(@PathVariable String id) {
-        var userResource = userService.findUser(id).map(this::toUserResource);
-        return userResource.orElseThrow(() -> new UserNotFoundException(id));
-    }
-
-    @PostMapping
-    public ResponseEntity<EntityModel<UserResource>> createUser(@RequestBody EntityModel<User> userResource) {
-
-        User                      createdUser       = userService.createUser(userResource.getContent());
-        EntityModel<UserResource> createdUserEntity = toUserResource(createdUser);
-        return ResponseEntity.status(HttpStatus.CREATED).body(createdUserEntity);
+        return pagedModel;
     }
 
     private EntityModel<UserResource> toUserResource(User u) {
