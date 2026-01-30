8fd99a9dc8a2fe526185f1fafe93d3a32b0ff80a
Ricardo da Silva Lima <rslima@gmail.com>
2026-01-30 17:26:37 -0300

    add support for creating child accounts, including UI integration, filtering valid parent accounts, and translations for English and Portuguese

--- api/src/main/resources/db/tree.sql
+++ api/src/main/resources/db/tree.sql
@@ -1,5 +1,5 @@
 with recursive tree as (
     select id, name::text, accounts.parent_account_id, 1 as level from accounts where parent_account_id is null
     union all
-    select accounts.id, (tree.name || ':' || accounts.name)::text, accounts.parent_account_id, (tree.level + 1) as level from accounts join tree on accounts.parent_account_id = tree.id
+    select accounts.id, (tree.name || ' > ' || accounts.name)::text, accounts.parent_account_id, (tree.level + 1) as level from accounts join tree on accounts.parent_account_id = tree.id
 ) select * from tree order by name;
