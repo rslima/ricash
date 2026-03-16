2cedf74f0b30564dfa36d33a869fdd4f3d489b33
Ricardo da Silva Lima <rslima@gmail.com>
2026-03-16 15:34:49 -0300

    add i18n import to test setup for localization support in tests

--- frontend/src/test/setup.ts
+++ frontend/src/test/setup.ts
@@ -1,6 +1,7 @@
 import "@testing-library/jest-dom/vitest"
 import { cleanup } from "@testing-library/react"
 import { afterEach, vi } from "vitest"
+import "@/i18n"
 
 // Cleanup after each test
 afterEach(() => {
