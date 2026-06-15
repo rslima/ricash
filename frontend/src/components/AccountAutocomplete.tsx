95a6a7608e874f3137b1670d0336bf71d852dd02
Ricardo da Silva Lima <rslima@senado.leg.br>
2026-06-15 09:48:44 -0300

    Standardize `PopoverContent` width in autocomplete components

--- frontend/src/components/AccountAutocomplete.tsx
+++ frontend/src/components/AccountAutocomplete.tsx
@@ -144,7 +144,7 @@ export function AccountAutocomplete({
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[400px] p-0" align="start">
+      <PopoverContent className="w-100 p-0" align="start">
         <Command>
           <CommandInput placeholder={t("transactions.searchAccounts")} />
           <CommandList>
--- frontend/src/components/DescriptionAutocomplete.tsx
+++ frontend/src/components/DescriptionAutocomplete.tsx
@@ -99,7 +99,7 @@ export function DescriptionAutocomplete({
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[400px] p-0" align="start">
+      <PopoverContent className="w-100 p-0" align="start">
         <Command shouldFilter={false}>
           <CommandInput
             placeholder={effectivePlaceholder}
