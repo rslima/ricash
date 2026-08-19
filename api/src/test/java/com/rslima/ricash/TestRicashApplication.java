dc943be0733ee13a8d3573f527a07d117c5deeba
Ricardo da Silva Lima <rslima@gmail.com>
2026-08-19 16:37:11 -0300

    refactor(tests): switch PostgreSQLContainer import to correct package
    Adjusted PostgreSQLContainer import from `org.testcontainers.containers` to `org.testcontainers.postgresql`. Removed wildcard type argument from Bean definition.

--- api/src/test/java/com/rslima/ricash/TestRicashApplication.java
+++ api/src/test/java/com/rslima/ricash/TestRicashApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 import java.util.List;
@@ -19,8 +19,8 @@ public class TestRicashApplication {
 
 	@Bean
 	@ServiceConnection
-	PostgreSQLContainer<?> postgresContainer() {
-		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:18.1"));
+	PostgreSQLContainer postgresContainer() {
+		return new PostgreSQLContainer(DockerImageName.parse("postgres:18.1"));
 	}
 
 	@Bean
